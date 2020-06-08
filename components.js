96c71d2d8d9b119749ecbb643665730feccde84b
LachlanDK <lachlandufort-kennett@outlook.com>
2020-06-08 15:04:45 +0100

    Created sidebar

--- components.js
+++ components.js
@@ -2,6 +2,12 @@ document.querySelectorAll(".numeric").forEach(function(input){
 	input.addEventListener("input", forceNumeric);
 });
 
+const sidebar = document.getElementById("sidebar")
+
+document.getElementById("sidebar-toggle").addEventListener("click", function(){
+	sidebar.classList.toggle("active");
+})
+
 function forceNumeric(event){
 	event.target.value = event.target.value.replace(/[^0-9-]/g, "");
 }
