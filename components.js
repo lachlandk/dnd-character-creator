2893dd4a62e5feede4128088cb89c85fbc9fd53b
LachlanDK <lachlandufort-kennett@outlook.com>
2020-06-09 20:29:06 +0100

    Added code for tooltip elements.

--- components.js
+++ components.js
@@ -1,13 +1,35 @@
 document.querySelectorAll(".numeric").forEach(function(input){
-	input.addEventListener("input", forceNumeric);
+	input.addEventListener("input", function(event){
+		event.target.value = event.target.value.replace(/[^0-9-]/g, "");
+	});
 });
 
-const sidebar = document.getElementById("sidebar")
+const sidebar = document.getElementById("sidebar");
 
 document.getElementById("sidebar-toggle").addEventListener("click", function(){
 	sidebar.classList.toggle("active");
-})
+});
 
-function forceNumeric(event){
-	event.target.value = event.target.value.replace(/[^0-9-]/g, "");
-}
+const tooltip = document.createElement("div");
+tooltip.style.display = "none";
+tooltip.style.position = "absolute";
+tooltip.style.backgroundColor = "#eeeeee";
+tooltip.style.border = "2px black solid";
+tooltip.style.borderRadius = "10px";
+tooltip.style.padding = "1% 2%";
+tooltip.style.zIndex = "10";
+document.body.appendChild(tooltip);
+
+document.querySelectorAll("[data-tooltip]").forEach(function(element){
+	element.addEventListener("mouseenter", function(event){
+		tooltip.style.left = event.clientX + event.target.offsetWidth/2 + "px";
+		tooltip.style.top = event.clientY + "px";
+		tooltip.style.display = "block";
+		tooltip.innerHTML = event.target.getAttribute("data-tooltip");
+	});
+
+	element.addEventListener("mouseleave", function(){
+		tooltip.style.display = "none";
+		tooltip.innerHTML = "";
+	});
+});
